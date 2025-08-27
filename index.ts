a305dbb925a553a382cf6eb8bf8180639621ba44
Robert Willemelis <robert.dev@gmx.de>
2025-08-27 13:19:45 +0200

    feat: add links from google sheet

--- index.ts
+++ index.ts
@@ -1,29 +1,50 @@
+import { execSync } from 'child_process';
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
+const SHEET_ID = "1Q3oGpVqS8k_PV9unRqxYvKFDF0q5qtah2-2QG36K_-c";
+const url = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json`;
 
-const redirects = [
-  { from: '/home', to: '/' },
-  { from: '/kiezbeirat', to: 'https://adlerkiez.de/' },
-];
+const data = execSync(`curl -s "${url}"`).toString();
+const json = JSON.parse(data.substring(47, data.length - 2));
+console.log(json.table.cols);
+console.log(json.table.rows);
+const targets = [];
+const colNames = json.table.cols[0].label !== '' ? json.table.cols.map((c: any) => c.label) : json.table.rows[0].c.map((_: any, i: number) => `${_.v}`);
+for (const row of json.table.rows) {
+    const entry: any = {};
+    row.c.forEach((cell: any, i: number) => {
+        const value = cell ? cell.v : null;
+        if(value === colNames[i]) return; // skip header row
+        entry[colNames[i]] = cell ? cell.v : null;
+    });
+    if(entry.name && entry.ziel && entry.status) targets.push({from: `/${entry.name}`, to: entry.ziel, status: entry.status});
+    console.log(entry);
+}
+
+// const redirects = [
+//   { from: '/home', to: '/' },
+//   { from: '/kiezbeirat', to: 'https://adlerkiez.de/' },
+// ];
 const result: any = {
   "version": 3,
   "routes": [
     { "src": "^/$", "dest": "/index.html" },
   ]
 }
-for (const { from, to } of redirects) {
-    if(to.startsWith('http')) {
+for (const entry of targets) {
+    if(entry.status === 'created') continue; // skip created entries
+    if(entry.to.startsWith('http')) {
         result.routes.push({
-            "src": `^${from}$`,
-            "status": 308,
+            "src": `^${entry.from}$`,
+            "status": entry.status === 'active' ? 301 : 404,
             "headers": { 
-                "Location": to,
+                "Location": entry.to,
                 "cache-control": "public, max-age=31536000, immutable"
             }
         });
     } else {
         // TODO: check for "/" and existance
-        result.routes.push({ "src": `^${from}$`, "dest": to });
+        result.routes.push({ "src": `^${entry.from}$`, "dest": entry.to, "status": entry.status === 'active' ? 301 : 404 });
     }
 }
 result.routes.push({ "handle": "filesystem" })
@@ -43,4 +64,4 @@ writeFileSync(`${DIR_OUTPUT}/${FILE_CONFIG}`, JSON.stringify(result, null, 2));
 if(!existsSync(DIR_DATA)) {
     mkdirSync(DIR_DATA, { recursive: true });
 }
-writeFileSync(`${DIR_DATA}/${FILE_LINKS}`, JSON.stringify(redirects, null, 2));
+writeFileSync(`${DIR_DATA}/${FILE_LINKS}`, JSON.stringify(targets, null, 2));
