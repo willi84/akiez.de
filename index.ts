22fb833f97270411bdd0491f6229e7de4af02a24
Robert Willemelis <robert.dev@gmx.de>
2025-08-27 10:55:47 +0200

    feat: add simple redirect

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
-const i:number = 'string';
-console.log('hello world' + i);
+import { writeFileSync } from 'fs';
+
+
+const redirects = [
+  { from: '/internal', to: '/' },
+  { from: '/kiezbeirat', to: 'https://adlerkiez.de/' },
+];
+const result: any = {
+    "$schema": "https://openapi.vercel.sh/vercel.json",
+    "redirects": []
+}
+
+for (const { from, to } of redirects) {
+    result.redirects.push({ source: from, destination: to, permanent: true });
+}
+const FILE = 'vercel.json';
+writeFileSync(FILE, JSON.stringify({ redirects }, null, 2));
